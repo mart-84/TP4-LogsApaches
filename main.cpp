aa4802167333945a698bc32bf11a71d8b93e6a78
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-02-06 12:43:51 +0100

    Ajout de la structure de tous les tests

--- main.cpp
+++ main.cpp
@@ -2,41 +2,8 @@
 #include <fstream>
 using namespace std;
 
-#include "AnalyseurLogs.h"
-#include "GenerateurGraphe.h"
 #include "VerificateurCommande.h"
-#include "Statistiques.h"
 #include "Controleur.h"
-
-void test1() {
-    ifstream fichier("anonyme.log");
-    AnalyseurLogs anal(fichier);
-    anal.LireFichier();
-
-    Statistiques stat(anal.GetLogs());
-    stat.CalculerStatistiques();
-    auto result = stat.GetTop10();
-    for (multimap<int, string>::reverse_iterator it = result.rbegin();
-         it != result.rend();
-         ++it)
-    {
-        cout << it->second << "\t " << it->first << " hits" << endl;
-    }
-}
-
-void test2()
-{
-    ifstream fichier("anonyme.log");
-    AnalyseurLogs anal(fichier);
-    anal.LireFichier();
-
-    Statistiques stat(anal.GetLogs());
-    stat.CalculerStatistiques();
-
-    ofstream o("graph.dot");
-    GenerateurGraphe gg(o, stat.GetGraph());
-    gg.ExporterGraphe();
-}
 
 int main(int argc, char **argv)
 {
@@ -45,5 +12,7 @@ int main(int argc, char **argv)
     {
         Controleur controleur(commande);
         controleur.Demarrer();
+        return 0;
     }
+    return 1;
 }
